51ff587a9aa99066dee4439a5cae9c3e0704d6ff
tomer644 <tomtom6758@gmail.com>
2022-11-24 08:04:48 +0200

    added a starting (basic) animation

---
+++ app/src/main/java/com/example/utimatetictactoe/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.utimatetictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView appname;
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        appname = findViewById(R.id.appname);
+        lottie = findViewById(R.id.lottie);
+
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        appname.animate().translationY(2000).setDuration(2000).setStartDelay(2900);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 5000);
+    }
+}
